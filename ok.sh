49a15532e02a3befc22e5e191a0b2c1329509806
wangqiang99992 <103892434+wangqiang99992@users.noreply.github.com>
2023-09-29 21:47:38 +0800

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-# 获取用户输入的ETH地址
-read -p "请输入您的ETH地址: " eth_address
+# Get the user's input for the ETH address
+read -p "Please enter your ETH address: " eth_address
 
-# 更新系统包
+# Update system packages
 sudo apt update
 
-# 安装所需的软件包
+# Install required software packages
 sudo apt install -y cmake python3-pip screen
 
-# 克隆GitHub仓库
+# Clone the GitHub repository
 if [ ! -d "XENGPUMiner" ]; then
     git clone https://github.com/shanhaicoder/XENGPUMiner
 else
-    echo "XENGPUMiner已存在，跳过克隆步骤"
+    echo "XENGPUMiner already exists, skipping the clone step."
 fi
 
-# 进入目录
+# Navigate into the directory
 cd XENGPUMiner/
 
-# 修改执行权限并编译
+# Change permissions and compile
 chmod +x build.sh
 ./build.sh -cuda_arch sm_70
 
-# 安装Python依赖
+# Install Python dependencies
 pip3 install -U -r requirements.txt
 
-# 使用screen在后台运行miner.py
+# Run miner.py in the background using screen
 screen -S miner -dm python3 miner.py --gpu=true --account=$eth_address
 
-# 使用for循环运行xengpuminer三次并将每次的输出重定向到不同的日志文件
+# Use a for loop to run xengpuminer three times and redirect each output to a different log file
 for i in {1..3}
 do
     nohup ./xengpuminer -b 1024 &> output_$i.log &
 done
 
-echo "安装和编译完成，miner.py已在screen会话中开始运行！"
+echo "Installation and compilation completed, miner.py is now running in a screen session!"
